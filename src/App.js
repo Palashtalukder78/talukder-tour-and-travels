63dafeff72c50c496de75e29c871860752ae1d21
Palashtalukder78 <palashtalukder78@gmail.com>
2021-10-28 23:28:45 +0600

    Setup environment with firebase,reactBootstrap,react router

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Button from 'react-bootstrap/Button'
+function App() {
+  return (
+    <div className="App">
+      <h1>ASSIGNMENT 11</h1>
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
